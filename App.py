bb76e8e5e2bb7162b5720f48fcf3b5d77e6fd71d
viceitec <vicedirector@itecriocuarto.org.ar>
2019-05-22 18:54:39 -0300

    Update App.py
    ...

--- App.py
+++ App.py
@@ -32,7 +32,7 @@ def add_contact():
         cur.execute('INSERT INTO contacts (fullname, phone, email) VALUES (%s, %s, %s)',
         (fullname, phone, email))
         mysql.connection.commit()
-        flash('Contacto Added Succesfully')
+        flash('Contacto Agregado')
         return redirect(url_for('Index')) 
 
 @app.route('/edit/<id>', methods = ['POST', 'GET'])
